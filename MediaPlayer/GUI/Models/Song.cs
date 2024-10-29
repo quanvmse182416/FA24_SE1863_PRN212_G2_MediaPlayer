dd5616e5569e7634ed82e26b2e3cc0e1f0528583
quanvmse182416 <quanvmse182416@fpt.edu.vn>
2024-10-29 17:48:31 +0700

    Added song and playlist model

---
+++ MediaPlayer/GUI/Models/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Models
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string FilePath { get; set; }
+
+        public Song(string title, string filePath)
+        {
+            Title = title;
+            FilePath = filePath;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
