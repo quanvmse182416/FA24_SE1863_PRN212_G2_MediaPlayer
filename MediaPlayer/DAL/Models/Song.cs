a2c9f795be6aff627b9da9d761306ac3717c8859
Sakamoto2004 <phbtoan9185@gmail.com>
2024-11-04 17:16:32 +0700

    Added Song and playlist model

---
+++ MediaPlayer/DAL/Models/Song.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer.DAL.Models;
+
+public class Song
+{
+    public string Title { get; set; }
+    public string FilePath { get; set; }
+    public string Duration { get; set; }
+    public string Artist { get; set; }
+    public string Album { get; set; }
+
+    public Song(string title, string filePath)
+    {
+        Title = title;
+        FilePath = filePath;
+    }
+
+    public override string ToString()
+    {
+        return Title;
+    }
+
+    public bool IsEquals( Song song)
+    {
+        int condition = 0;
+        if (this.Title == song.Title) ++condition;
+        if (this.FilePath == song.FilePath) ++condition;
+        if (condition == 2)
+            return true;
+        return false;
+    }
+
+}
